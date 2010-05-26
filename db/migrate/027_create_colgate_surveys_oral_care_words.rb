622fa56fe414343ac47341ce27ed2b1ddece7aff
rodrigo brancher <brancher@server.aquinanu.com.br>
2010-05-26 11:23:58 -0300

    :false -> false no db migrate

--- db/migrate/027_create_colgate_surveys_oral_care_words.rb
+++ db/migrate/027_create_colgate_surveys_oral_care_words.rb
@@ -1,6 +1,6 @@
 class CreateColgateSurveysOralCareWords < ActiveRecord::Migration
   def self.up
-    create_table :colgate_surveys_oral_care_words, :id=>:false do |t|
+    create_table :colgate_surveys_oral_care_words, :id=>false do |t|
       t.references :colgate_survey
       t.references :oral_care_word
     end
--- db/migrate/029_create_colgate_surveys_samplings.rb
+++ db/migrate/029_create_colgate_surveys_samplings.rb
@@ -1,6 +1,6 @@
 class CreateColgateSurveysSamplings < ActiveRecord::Migration
   def self.up
-    create_table :colgate_surveys_samplings, :id=>:false do |t|
+    create_table :colgate_surveys_samplings, :id=>false do |t|
       t.references :colgate_survey
       t.references :sampling
     end
