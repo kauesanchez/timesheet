0596f53c586830f83c4d130395ed9ef53e80cd23
kaue sanchez <kaue.sanchez@nudesign.com.br>
2010-07-02 15:33:18 -0300

    mod finais2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   # protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
+  #  campo select , que lista os projetos no periodo. 
+  #  arrumar relacionamentos
+  #  arrumar campo de horas e soma-las. 
+  #  calendario e datas (pensar na melhor forma)  
+
   filter_parameter_logging :password
   
   def login_required
